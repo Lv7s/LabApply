fbc2abde2fb12c6a6172d5439ef7bc4e51fd9874
Luis Valverde <luvalfer2009@hotmail.com>
2022-08-02 01:50:01 -0300

    Initial commit

---
+++ app/src/main/java/com/lav7/labapply/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lav7.labapply
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
