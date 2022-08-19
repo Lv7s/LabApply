fedf9589c81b2814ec48c4867e765eabd7c4580f
Luis Valverde <luvalfer2009@hotmail.com>
2022-08-19 00:50:05 -0300

    adicionadas las operaciones de TRIS

--- app/src/main/java/com/lav7/labapply/Fragment_c.kt
+++ app/src/main/java/com/lav7/labapply/Fragment_c.kt
@@ -1,10 +1,13 @@
 package com.lav7.labapply
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main. *
+import kotlinx.android.synthetic.main.fragment_c.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,19 +20,75 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Fragment_c.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment_c : Fragment() {
+class Fragment_c : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    var VolReq : Float? = null
+    var ConcTris : Float? = null
+    var ConcNaCl : Float? = null
+    var ConcMgCl2 : Float? = null
+    var VolGlic : Float? = null
+    val TRIS = 121.14
+    val NaCl = 58.44
+    val Hepes = 238.30
+    val MgCl2Anhidro = 95.21
+    val MgCl2Hexahidro = 203.31
+    val Glicerol = 1.26
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
+            calcularTris.setOnClickListener { this }
+            arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
+    override fun onClick(p0: View?) {
+        if  (TextUtils.isEmpty(editTextTextPersonName.text.toString())){
+            editTextTextPersonName.error = "Ingresa un numero"
+            editTextTextPersonName.requestFocus()
+            return
+        }else {
+            VolReq = editTextTextPersonName.text.toString().toFloat()
+        }
+        if  (TextUtils.isEmpty(editTextTextPersonName2.text.toString())){
+            editTextTextPersonName2.error = "Ingresa un numero"
+            editTextTextPersonName2.requestFocus()
+            return
+        }else {
+            ConcTris = editTextTextPersonName2.text.toString().toFloat()
+        }
+        if  (TextUtils.isEmpty(editTextTextPersonName3.text.toString())){
+            editTextTextPersonName3.error = "Ingresa un numero"
+            editTextTextPersonName3.requestFocus()
+            return
+        }else {
+            ConcNaCl = editTextTextPersonName3.text.toString().toFloat()
+        }
+        if  (TextUtils.isEmpty(editTextTextPersonName5.text.toString())){
+            editTextTextPersonName5.error = "Ingresa un numero"
+            editTextTextPersonName5.requestFocus()
+            return
+        }else {
+            ConcMgCl2 = editTextTextPersonName5.text.toString().toFloat()
+        }
+        if  (TextUtils.isEmpty(editTextTextPersonName4.text.toString())){
+            editTextTextPersonName4.error = "Ingresa un numero"
+            editTextTextPersonName4.requestFocus()
+            return
+        }else {
+            VolGlic = editTextTextPersonName4.text.toString().toFloat()
+        }
+        when(p0?.id){
+            R.id.calcularTris -> {textView.text=ConcTris!!.times(VolReq!!).times(TRIS).toString()}
+            R.id.calcularTris -> {textView2.text=ConcNaCl!!.times(VolReq!!).times(NaCl).toString()}
+            R.id.calcularTris -> {textView3.text=ConcMgCl2!!.times(VolReq!!).times(MgCl2Hexahidro).toString()}
+            R.id.calcularTris -> {textView8.text=VolGlic!!.times(Glicerol).toString()}
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/lav7/labapply/MainActivity.kt
+++ app/src/main/java/com/lav7/labapply/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main. *
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
